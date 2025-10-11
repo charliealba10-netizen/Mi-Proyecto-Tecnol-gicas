0c7b9f7777919bed49985c191f2f36302f2a49c7
Carlos Alba <charlie.alba10@gmail.com>
2025-10-11 17:06:53 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+// Cambiar secciones
+function mostrar(id) {
+  document.querySelectorAll('section').forEach(sec => sec.classList.remove('active'));
+  document.querySelectorAll('nav button').forEach(btn => btn.classList.remove('active'));
+  document.getElementById(id).classList.add('active');
+  event.target.classList.add('active');
+}
+
+// Modo oscuro
+const darkBtn = document.getElementById('darkModeBtn');
+darkBtn.addEventListener('click', () => {
+  document.body.classList.toggle('dark-mode');
+  if(document.body.classList.contains('dark-mode')){
+    darkBtn.textContent = "Modo Claro ☀️";
+  } else {
+    darkBtn.textContent = "Modo Oscuro 🌙";
+  }
+});
+
 
